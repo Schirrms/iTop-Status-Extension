c6cdfe6def9a54366d34a22d85d44360eccbf365
Schirrms <48877593+Schirrms@users.noreply.github.com>
2022-02-22 18:39:45 +0100

    First upload

---
+++ schirrms-update-status/fr.dict.schirrms-update-status.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2022 M. CAMARA
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('FR FR', 'French', 'French', array(
+  // Dictionary entries go here
+  'Class:PhysicalDevice/Attribute:status/Value:decommissioning' => 'en cours de décommissionnement',
+  'Class:VirtualDevice/Attribute:status/Value:decommissioning' => 'en cours de décommissionnement',
+));
+?>
